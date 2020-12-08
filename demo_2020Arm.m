8e1993403a260f18b067b9bd02c7b94bc102d0c3
athanxenos <56540839+athanxenos@users.noreply.github.com>
2020-12-08 17:33:21 +1100

    Update demo_2020Arm.m

--- demo_2020Arm.m
+++ demo_2020Arm.m
@@ -7,18 +7,18 @@ close all;
 g = ncgr_graphic();
 
 global N_DOFS;
-N_DOFS = 6;
+N_DOFS = 7;
 
 %% 6-R 2020 Arm DH-Parameters
-theta = [0 0 0 0 0 0];
-alpha = [0 pi/2 0 0 -pi/2 -pi/2];
-offset = [0 pi/2 -pi/2 0 -pi/2 0];
-d = [0 0 0 0 110 0];
-a = [0 0 650 415 0 0];
-type = ['r','r','r','r','r','r'];
+theta = [0 0 0 0 0 0 0];
+alpha = [0 pi/2 0 0 -pi/2 -pi/2 0];
+offset = [0 pi/2 -pi/2 0 -pi/2 0 0];
+d = [0 100 0 0 105 0 305];
+a = [0 0 520 495 0 0 0];
+type = ['r','r','r','r','r','r','r'];
 base = [0; 0; 0];
 
 planar_6r = cgr_create(theta, d, a, alpha, offset, type, base, ...
-    [-pi; -pi; -pi; -pi; -pi; -pi], [pi; pi; pi; pi; pi; pi]);  % joint limts!
-planar_6r = cgr_self_update(planar_6r, [0; 0; 0; 0; 0; 0;], base);
+    [-pi; -pi; -pi; -pi; -pi; -pi; -pi], [pi; pi; pi; pi; pi; pi; pi]);  % joint limts!
+planar_6r = cgr_self_update(planar_6r, [0; 0; 0; 0; 0; 0; 0], base);
 g = ncgr_plot(g, planar_6r, [1 1 1], 1);
